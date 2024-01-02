d3591825e60fb64dfeca23cc86b7159312092a49
bradburp <paul.bradbury@live.co.uk>
2024-01-02 14:36:42 +0000

    Initial commit

---
+++ src/main/java/com/bradburp/playground/Main.java
@@ -0,0 +1,24 @@
+package com.bradburp.playground;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Price of a Calzone pizza in South Africa will be: " + covertPoundsToRand("calzone", 15.00));
+
+
+
+        String name = "Paul";
+        String name1 = new String("Paul");
+
+        System.out.println(name);
+        System.out.println(name1);
+    }
+
+
+
+    public static Double covertPoundsToRand(String pizza, Double rand) {
+
+        return 10.0;
+    }
+
+
+}
